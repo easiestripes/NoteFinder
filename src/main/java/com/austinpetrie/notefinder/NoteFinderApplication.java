4ca5a4b9bf43eb6fc38a21e94c4986dd14233a1c
easiestripes <adp4fm@virginia.edu>
2017-03-06 13:44:50 -0800

    Added .gitignore

--- src/main/java/com/austinpetrie/notefinder/NoteFinderApplication.java
+++ src/main/java/com/austinpetrie/notefinder/NoteFinderApplication.java
@@ -1,10 +1,19 @@
 package com.austinpetrie.notefinder;
 
+import com.austinpetrie.notefinder.rest.controller.NotesRESTController;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
+import org.glassfish.jersey.jaxb.internal.XmlCollectionJaxbProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.austinpetrie.notefinder.rest.controller.NotesRESTController;
+
 public class NoteFinderApplication extends Application<NoteFinderConfiguration> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(XmlCollectionJaxbProvider.App.class);
 
     public static void main(final String[] args) throws Exception {
         new NoteFinderApplication().run(args);
@@ -22,8 +31,9 @@ public class NoteFinderApplication extends Application<NoteFinderConfiguration> 
 
     @Override
     public void run(final NoteFinderConfiguration configuration,
-                    final Environment environment) {
-        // TODO: implement application
+                    final Environment env) {
+        LOGGER.info("Registering REST resources");
+        env.jersey().register(new NotesRESTController(env.getValidator()));
     }
 
 }
